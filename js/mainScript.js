3ca6c689eabb2aca56d0d63c791f536691b3adcd
Lucas <lucasgether@gmail.com>
2020-10-10 14:01:30 +0200

    debugging2

--- js/mainScript.js
+++ js/mainScript.js
@@ -72,16 +72,3 @@ function mute() {
     }
 
 }
-
-document.getElementById('sButton').onclick = function() {
-    var mutedEl = document.getElementById("muted");
-    //var sButton = document.getElementById("sButton");
-    if (sButton.innerHTML == "Mute device") {
-        mutedEl.innerHTML = "Your device is now muted"
-        sButton.innerHTML = "Unmute device";
-        console.log("Device now being muted");
-    } else {
-        mutedEl.innerHTML = "Your device is now unmuted"
-        sButton.innerHTML = "Mute device";
-    }
-};
