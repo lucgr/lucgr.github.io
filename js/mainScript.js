729f267adb167657308ed94c86aea117d9ae033d
Lucas <lucasgether@gmail.com>
2020-10-12 11:34:26 +0200

    temporarily attach first sound

--- js/mainScript.js
+++ js/mainScript.js
@@ -52,3 +52,13 @@ function pair() {
         response.innerHTML = "You already have a device paired";
     }
 }
+
+function playClassical() {
+    var sound = new Audio("/Users/20190860/Documents/GitHub/lucgr.github.io/js/media/classical.mp3");
+    console.log('Sound clicked');
+    sound.play()
+    setTimeout(function() {
+        sound.pause();
+        sound.currentTime = 0;
+    }, 15000);
+}
